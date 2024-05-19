ff9653200501d24fb4a63ae172f47b2e5bf7fe0d
yulianto <jooliantoo@gmail.com>
2024-05-19 10:59:28 +0700

    update function2

--- function.js
+++ function.js
@@ -15,10 +15,10 @@ const ekspresi = function () {
 };
 
 //* 3. arrow function
-// - jika hanya 1 parameter, tidak wajib dibungkus kurung ()
-// - jika parameter lebih dari 1, wajib dibungkus kurung ()
-// - jika tidak ada paramter, tetap wajib menggunakan kurung ()
-// - konsep this akan berbeda ketika kita menggunakan arrow function
+//* - jika hanya 1 parameter, tidak wajib dibungkus kurung ()
+//* - jika parameter lebih dari 1, wajib dibungkus kurung ()
+//* - jika tidak ada paramter, tetap wajib menggunakan kurung ()
+//* - konsep this akan berbeda ketika kita menggunakan arrow function
 const panah1 = (a, b) => {
   document.write("arrow function: ");
   document.write(`${a} + ${b} = ${a + b}`);
